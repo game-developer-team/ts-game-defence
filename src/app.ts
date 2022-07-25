6e1fb278a5f0b0f836d5e2ceb942d030859c98fb
LacoLi <gomsemarie@gmail.com>
2022-07-25 15:24:19 +0900

    삭제 커밋

--- src/app.ts
+++
@@ -1,9 +0,0 @@
-import express, {Request, Response, NextFunction } from 'express';
-
-const app = express();
-
-app.get('/root', (req: Request, res: Response, next: NextFunction) => res.send('Root!!'));
-
-app.listen('3002', () => {
-    console.log('Start port 3002')
-});
