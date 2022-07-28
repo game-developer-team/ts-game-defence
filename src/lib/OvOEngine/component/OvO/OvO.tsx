d844c0bc45c7b6ee5d27cef50ab9aeb9b7ea47b0
LacoLi <gomsemarie@gmail.com>
2022-07-28 16:47:05 +0900

    ref 약간 수정

--- src/lib/OvOEngine/component/OvO/OvO.tsx
+++ src/lib/OvOEngine/component/OvO/OvO.tsx
@@ -12,14 +12,25 @@ import _useOvO from './hook/useOvO';
 import _useOvOInner from './hook/useOvOInner';
 
 interface OvOProps {
+  width: number;
+  height: number;
   children?: React.ReactNode;
 }
 
 function OvO(props: OvOProps) {
-  const { children } = props;
+  const { width, height, children } = props;
 
   const canvasRef = React.useRef<HTMLCanvasElement>(null);
   const useOvO = _useOvO(canvasRef);
+
+  React.useEffect(() => {
+    if (canvasRef.current !== null) {
+      // Canvas size 설정
+      const canvas = canvasRef.current;
+      canvas.width = width;
+      canvas.height = height;
+    }
+  }, [width, height]);
 
   return (
     <div data-component="ovO">
